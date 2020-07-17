a56059454d92bc541d2204fd842c7082e541809d
Gerard Eklu <gerard_eklu@homedepot.com>
2020-07-17 00:26:52 -0400

    Laid down the html and data to work with on the soccer legends app

---
+++ app.js
@@ -0,0 +1,22 @@
+// Local Legends Data
+
+const legends = [
+  {
+    id: 1,
+    city: "Roma=e",
+    img: "https://www.hellomagazine.com/imagenes/travel/2018061449462/things-to-do-in-rome-3-days/0-243-749/Colosseum-Rome-t.jpg",
+    details: "Rome, capital of Italy"
+  },
+  {
+    id: 2,
+    city: "Rio De Janeiro",
+    img: "https://www.jetex.com/wp-content/uploads/2019/12/rio-de-janeiro-brazil-1000-667.jpg",
+    details: "Rio, capital of Brazil"
+  },
+  {
+    id: 3,
+    city: "Lome",
+    img: "https://i.ytimg.com/vi/KO_Tu7WCOH8/maxresdefault.jpg",
+    details: "Rome, capital of Togo"
+  }
+]
